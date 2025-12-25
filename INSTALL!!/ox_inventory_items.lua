70a5a64266893d483c93eccbb5e95035e251c4bc
Cxsper <cxsperdev@gmail.com>
2025-12-25 01:06:01 +0000

    Added inventory items to install folder

---
+++ INSTALL!!/ox_inventory_items.lua
@@ -0,0 +1,52 @@
+    ['bp_weapon_pistol'] = {
+        label = 'Pistol Blueprint',
+        description = 'Learn how to craft a standard pistol.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+    
+    ['bp_weapon_smg'] = {
+        label = 'SMG Blueprint',
+        description = 'Learn how to craft a submachine gun.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+
+    ['bp_repairkit'] = {
+        label = 'Repair Kit Blueprint',
+        description = 'Learn how to craft a repair kit.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+
+    ['bp_armour'] = {
+        label = 'Armour Blueprint',
+        description = 'Learn how to craft an armor plate.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+    ['bp_advancedlockpick'] = {
+        label = 'Advanced Lockpick Blueprint',
+        description = 'Learn how to craft an advanced lockpick.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+    ['bp_thermite'] = {
+        label = 'Thermite Blueprint',
+        description = 'Learn how to craft thermite.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
+    ['bp_lockpick'] = {
+        label = 'Lockpick Blueprint',
+        description = 'Learn how to craft a basic lockpick.',
+        weight = 0,
+        stack = false,
+        close = true,
+    },
